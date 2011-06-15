e02320739c8700c93b01d51dd79cc7d2a99753f3
Ciprian Dorin, Craciun <ciprian@volution.ro>
2011-06-15 20:16:26 +0300

    Minor script updates.

--- scripts/run-node.bash
+++ scripts/run-node.bash
@@ -9,7 +9,6 @@ _identifier="${1:-0000000000000000000000000000000000000000}"
 
 if test "${_identifier}" != 0000000000000000000000000000000000000000 ; then
 	_erl_argv=(
-		"${_erl}"
 			"${_erl_args[@]}"
 			-noinput -noshell
 			-sname "mosaic-httpg-${_identifier}@${_erl_host}" -setcookie "${_erl_cookie}"
@@ -18,15 +17,15 @@ if test "${_identifier}" != 0000000000000000000000000000000000000000 ; then
 			-run mosaic_component_app boot
 	)
 	_erl_env=(
-		mosaic_component_identifier="${_identifier}"
-		mosaic_component_harness_input_descriptor=3
-		mosaic_component_harness_output_descriptor=4
-		ERL_EPMD_PORT="${_erl_epmd_port}"
+			mosaic_component_identifier="${_identifier}"
+			mosaic_component_harness_input_descriptor=3
+			mosaic_component_harness_output_descriptor=4
+			ERL_EPMD_PORT="${_erl_epmd_port}"
 	)
 	exec  3<&0- 4>&1- </dev/null >&2
 else
+	_identifier=00000000ba8d275d66fd454594ee28264ab4a710
 	_erl_argv=(
-		"${_erl}"
 			"${_erl_args[@]}"
 			-noinput -noshell
 			-sname "mosaic-httpg-${_identifier}@${_erl_host}" -setcookie "${_erl_cookie}"
@@ -35,8 +34,11 @@ else
 			-run mosaic_httpg_callbacks standalone
 	)
 	_erl_env=(
-		ERL_EPMD_PORT="${_erl_epmd_port}"
+			ERL_EPMD_PORT="${_erl_epmd_port}"
 	)
 fi
 
-exec env "${_erl_env[@]}" "${_erl_argv[@]}"
+mkdir -p "/tmp/mosaic/components/${_identifier}"
+cd "/tmp/mosaic/components/${_identifier}"
+
+exec env "${_erl_env[@]}" "${_erl}" "${_erl_argv[@]}"
