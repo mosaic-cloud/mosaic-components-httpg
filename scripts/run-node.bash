17e7c04bee4ef74f9f7018c7d1686ca0d8c68789
Ciprian Dorin, Craciun <ciprian@volution.ro>
2011-08-31 15:06:48 +0300

    Added support for customizable temporary folder.

--- scripts/run-node.bash
+++ scripts/run-node.bash
@@ -8,6 +8,14 @@ fi
 _identifier="${1:-00000000ba8d275d66fd454594ee28264ab4a710}"
 _fqdn="${mosaic_node_fqdn:-}"
 
+if test -n "${mosaic_component_temporary:-}" ; then
+	_tmp="${mosaic_component_temporary:-}"
+elif test -n "${mosaic_node_temporary:-}" ; then
+	_tmp="${mosaic_node_temporary}/components/${_identifier}"
+else
+	_tmp="/tmp/mosaic/components/${_identifier}"
+fi
+
 _erl_args+=(
 		-noinput -noshell
 		-name "mosaic-httpg-${_identifier}@${_fqdn:-mosaic-0.loopback.vnet}"
@@ -17,6 +25,7 @@ _erl_args+=(
 )
 _erl_env+=(
 		mosaic_component_identifier="${_identifier}"
+		mosaic_component_temporary="${_tmp}"
 		mosaic_node_fqdn="${_fqdn}"
 )
 
@@ -35,7 +44,7 @@ else
 	)
 fi
 
-mkdir -p "/tmp/mosaic/components/${_identifier}"
-cd "/tmp/mosaic/components/${_identifier}"
+mkdir -p "${_tmp}"
+cd "${_tmp}"
 
 exec env "${_erl_env[@]}" "${_erl}" "${_erl_args[@]}"
